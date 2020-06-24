78d05c6db679b2f7adcc298579b3c7413d966c05
Lim Chorng UanE <limchornguan418@gmail.com>
2020-06-24 09:36:44 +0200

    Add test for FrequencyCounter

--- tests/FrequencyCounter.elm
+++ tests/FrequencyCounter.elm
@@ -2,8 +2,38 @@ module FrequencyCounter exposing (..)
 
 import Expect exposing (Expectation)
 import Test exposing (..)
+import Exercise.FrequencyCounter exposing (frequencyCounter)
 
 
 suite : Test
 suite =
-    todo "Implement our first test. See https://package.elm-lang.org/packages/elm-explorations/test/latest for how to do this!"
+    describe "Test FrequencyCounter"
+        [ test "should return empty list if the given list is empty" <|
+            \_ ->
+                (frequencyCounter [])
+                    |> Expect.equal []
+
+        , describe "Given a list of words, it should return a list of tuples with first value being the word and second value being the frequency of the word occurs in the list"
+            [ test "1." <|
+                \_ ->
+                    let
+                        words = ["Elm"]
+                        expected = [("Elm", 1)]
+                    in
+                        Expect.equal expected (frequencyCounter words)
+            , test "2." <|
+                \_ ->
+                    let
+                        words = ["Elm", "Javascript", "PHP", "Elm", "Javascript", "Elm"]
+                        expected = [("Elm", 3), ("Javascript", 2), ("PHP", 1)]
+                    in
+                        Expect.equal expected (frequencyCounter words)
+            , test "3." <|
+                \_ ->
+                    let
+                        words = ["Elm", "Javascript", "Javascript", "PHP"]
+                        expected = [("Elm", 1), ("Javascript", 2), ("PHP", 1)]
+                    in
+                        Expect.equal expected (frequencyCounter words)
+            ]
+        ]
